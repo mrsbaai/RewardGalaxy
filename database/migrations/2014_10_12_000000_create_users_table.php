f7cc03365320cb67c742bd7eda4d41a174256b16
mrsbaai <abdelilah.sbaai@gmail.com>
2018-01-23 13:15:11 +0000

    letstart

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
+            $table->string('name');
             $table->string('password');
+            $table->string('first_name')->nullable()->default(null);
+            $table->string('last_name')->nullable()->default(null);
+            $table->string('address1')->nullable()->default(null);
+            $table->string('address2')->nullable()->default(null);
+            $table->string('zip')->nullable()->default(null);
+            $table->string('city')->nullable()->default(null);
+            $table->string('country')->nullable()->default(null);
+            $table->string('phone')->nullable()->default(null);
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2018_01_23_124117_create_rewards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRewardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rewards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('short_description');
+            $table->string('long_desctiption');
+            $table->string('thumbnail')->default("thumb.png");
+            $table->integer('coins')->default(100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rewards');
+    }
+}
