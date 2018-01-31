538ca6f2f695cceaf5eaa0ea5d11445b6a75fe58
mrsbaai <abdelilah.sbaai@gmail.com>
2018-01-31 10:10:03 +0000

    firstbuild

--- resources/views/coins.blade.php
+++ resources/views/coins.blade.php
@@ -24,7 +24,7 @@
                         data-toggle="tooltip"
                         readonly="readonly"
                         title="Copy & Paste"
-                        value="http://{{substr (Request::root(), 7)}}/hop/{{auth::user()->refcode}}"
+                        value="https://rewardgalaxy.com/hop/{{auth::user()->refcode}}"
                         onfocus="this.select();"
                 >
 
@@ -36,7 +36,7 @@
     <div class="container width-fix col-sm-12">
         <div class="jumbotron welcome-texture">
             <h2>Earn Coins: Methode 2 (Surveys)</h2>
-            <iframe src="http://lockwall.xyz/wall/3RS"
+            <iframe src="https://lockwall.xyz/wall/3RS"
                     width="100%"
                     height="1000px"
                     style="border:0; padding:0; scrolling=auto; margin:0;" frameborder="0" />
