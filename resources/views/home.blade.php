afd35f459840826323752dacaf5c6d632d63aca5
mrsbaai <abdelilah.sbaai@gmail.com>
2018-05-22 19:03:53 +0000

    pro

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,8 +11,8 @@
 @section('content')
     <div class="container width-fix col-sm-12">
         <div class="jumbotron welcome-texture">
-            <h2>Welcome To Reward Galaxy!</h2>
-            <p>On this web site you will find some rewards. To win rewards, you need coins.</p>
+            <h2>It's Simple on Reward Galaxy:</h2>
+            <p><b>The products here cost <u>Coins</u> insted of <strike>Money</strike></b></p>
             <h2>How to earn Coins?</h2>
             <ul>
                 <li>
@@ -30,8 +30,11 @@
 
         </div>
     </div>
-
-
+    <div class="container col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+        <br/> <br/> <br/>
+        <a href="/register" type="button" class="btn btn-lg btn-warning">Get Instant Free Coins</a>
+        <br/> <br/> <br/>
+    </div>
 
     <div class="container col-sm-12">
         @yield('rewards')
@@ -40,10 +43,10 @@
 
     <div class="container col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
         <h2 style="color:white;">
-            Register Now And Get {{config('app.signupbonus')}} Coins!
+            Register Now And Get Instant Free Coins!
         </h2>
         <br/>
-        <a href="/register" type="button" class="btn btn-lg btn-primary">Get A Free Account</a>
+        <a href="/register" type="button" class="btn btn-lg btn-primary">Register Now</a>
         <br/> <br/> <br/>
     </div>
 
