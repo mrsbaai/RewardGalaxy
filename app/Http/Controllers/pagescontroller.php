a48d40b352d9d3d222b72a67a52445041dd5bfa8
mrsbaai <abdelilah.sbaai@gmail.com>
2018-05-23 19:17:15 +0000

    framguard

--- app/Http/Controllers/pagescontroller.php
+++ app/Http/Controllers/pagescontroller.php
@@ -30,7 +30,6 @@ class pagesController extends Controller
         return $_SESSION['referrer'];
     }
     public function home(){
-		return "test";
         if (Auth::check()){
             $this->updateIP();
             return view("coins");
