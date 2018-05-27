26b5f36d3c59527b57193d91dfa08962f11ac51f
mrsbaai <abdelilah.sbaai@gmail.com>
2018-05-27 21:53:57 +0000

    fast register form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,8 @@
 		It's simple at RewardGalaxy, all products cost <u>Coins</u> instead of <strike>Money</strike></b></p>
 
 
-                <form role="form" method="POST" action="{{ url('/') }}" id="register-form">
+
+            {{ Form::open(array('action' => 'userController@create', 'id' => 'register-form'))}}
                     <div class="container-fluid">
                         <div class="col-lg-2 col-md-1"> </div>
                         <div class="col-lg-8 col-md-10">
@@ -41,7 +42,7 @@
                         </div>
                     </div>
 
-                </form>
+            {{ Form::close() }}
 
 
 
